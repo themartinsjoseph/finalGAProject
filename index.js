8323f184e21a2443b8b9f42297c9afe6905a3042
themartinsjoseph <themartinsjoseph@gmail.com>
2017-04-17 11:46:08 -0700

    initial app skeleton set up - no database

---
+++ index.js
@@ -0,0 +1,29 @@
+//REQUIRES AND GLOBAL VARS
+var express = require('express');
+var bodyParser = require('body-parser');
+require("dotenv").config();
+var path = require('path');
+
+var app = express();
+
+// MONGOOSE MODELS & CONNECTION
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Project3');
+
+//USE & SET STATEMENTS
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('morgan')('dev'));
+
+//ROUTERS
+app.get("/*", function(req, res) {
+	res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+//CONTROLLERS
+
+//LISTENERS 
+var server = app.listen(process.env.PORT || 3000);
+module.exports = server;
